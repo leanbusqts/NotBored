688e8a0e590c92c845464638bf08508537ca9e49
Leandro Busquets <leandro.busquets@mercadolibre.com>
2022-03-18 15:40:01 -0300

    base first screen

--- app/src/main/java/dev/bulean/notbored/MainActivity.kt
+++ app/src/main/java/dev/bulean/notbored/MainActivity.kt
@@ -1,11 +1,29 @@
-package dev.bulean.notbored
+ package dev.bulean.notbored
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
+import dev.bulean.notbored.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.btnStart.setOnClickListener {
+            val participants = binding.etParticipants.text.toString()
+            if (participants.toInt() >= 1) {
+                // TODO ("otro activity")
+            }
+
+        }
+
+
     }
 }
