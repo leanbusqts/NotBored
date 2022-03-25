a0c1974454176f3959e010c5f400f1f4592f9d8e
AndresRangelMeli <andres.rangel@mercadolibre.com.co>
2022-03-25 11:24:15 -0500

    design modification

--- app/src/main/java/dev/bulean/notbored/main/MainActivity.kt
+++ app/src/main/java/dev/bulean/notbored/main/MainActivity.kt
@@ -1,9 +1,8 @@
 package dev.bulean.notbored.main
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import dev.bulean.notbored.R
 import dev.bulean.notbored.activities.ActivitiesActivity
--- app/src/main/java/dev/bulean/notbored/main/TermsConditionsActivity.kt
+++ app/src/main/java/dev/bulean/notbored/main/TermsConditionsActivity.kt
@@ -3,10 +3,19 @@ package dev.bulean.notbored.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.bulean.notbored.R
+import dev.bulean.notbored.databinding.ActivityActivitiesBinding
+import dev.bulean.notbored.databinding.ActivityTermsConditionsBinding
 
 class TermsConditionsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityTermsConditionsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_terms_conditions)
+        binding = ActivityTermsConditionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ibExitTermsConditions.setOnClickListener { finish() }
+
     }
 }
